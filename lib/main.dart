4d0703edfe0efafd9f890ce00f245b8ede0eaa3b
Jose Gustavo <josegustavovictor@gmail.com>
2026-03-19 17:54:50 -0300

    chore: register appointments and rating providers

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,12 @@ import 'repositories/appointment_repository.dart';
 import 'repositories/user_repository.dart';
 import 'repositories/service_type_repository.dart';
 import 'repositories/professional_repository.dart';
+import 'repositories/rating_repository.dart';
 import 'view_models/auth_view_model.dart';
 import 'view_models/home_view_model.dart';
 import 'view_models/profile_view_model.dart';
+import 'view_models/appointments_view_model.dart';
+import 'view_models/rating_view_model.dart';
 
 void main() {
   final apiService = ApiService();
@@ -24,6 +27,7 @@ void main() {
         Provider(create: (_) => UserRepository(apiService: apiService)),
         Provider(create: (_) => ServiceTypeRepository(apiService: apiService)),
         Provider(create: (_) => ProfessionalRepository(apiService: apiService)),
+        Provider(create: (_) => RatingRepository(apiService: apiService)),
         ChangeNotifierProvider(
           create: (context) => AuthViewModel(
             repository: context.read<AuthRepository>(),
@@ -37,6 +41,16 @@ void main() {
         ChangeNotifierProvider(
           create: (context) => ProfileViewModel(
             repository: context.read<UserRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => AppointmentsViewModel(
+            repository: context.read<AppointmentRepository>(),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => RatingViewModel(
+            repository: context.read<RatingRepository>(),
           ),
         ),
       ],
